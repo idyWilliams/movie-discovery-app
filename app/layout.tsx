ffd5735c4ba9373e12b9cdc8434c05274b8a3fc1
idyWilliams <widorenyin0@gmail.com>
2025-10-19 17:49:20 +0100

    UPDATES AND FIXES

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,12 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
-}
+  title: "Movie Discovry App",
+  description: "Created by Williams",
+  authors: [
+    { name: "Idorenyin Williams", url: "https://github.com/idyWilliams" },
+  ],
+};
 
 export default function RootLayout({
   children,
